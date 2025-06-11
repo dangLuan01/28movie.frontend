b1f23c2050b32d0fc30793d5ceaaa0d9a0b76584
naul99 <luanboss012@gmail.com>
2025-06-11 11:04:07 +0700

    fix owl-carousel components react

--- src/components/MovieTheme.tsx
+++ src/components/MovieTheme.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef, useCallback } from 'react';
-
+import OwlCarousel from 'react-owl-carousel';
 type Movie = {
   slug: string;
   image: { poster: string };
@@ -17,6 +17,7 @@ type ThemeData = {
     genre_id: number;
     type: string;
     limit: number;
+    layout: number;
   };
   data: {
     movies: Movie[];
@@ -115,24 +116,59 @@ export default function MovieThemeLoader() {
       }
     };
   }, [currentThemePage, totalThemePages, loadingThemes, fetchThemes]);
-
+  const options = {
+    mouseDrag: true,
+    touchDrag: true,
+    dots: true,
+    loop: true,
+    autoplay: false,
+    smartSpeed: 600,
+    margin: 20,
+    autoHeight: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      576: {
+        items: 3,
+      },
+      768: {
+        items: 3,
+        margin: 30,
+      },
+      992: {
+        items: 4,
+        margin: 30,
+      },
+      1200: {
+        items: 6,
+        margin: 30,
+        dots: false,
+        mouseDrag: false,
+        slideBy: 6,
+        smartSpeed: 400,
+      },
+    },
+  };
   return (
-    <div className="theme-loader-container">
-      {visibleThemes.map((theme) => (
-        <div key={theme.theme.id} className="catalog">
+    <>
+      {visibleThemes.map((items) => (
+        <>
+        {items.theme.layout === 1 ? (
+        <div key={items.theme.id} className="catalog">
           <div className="container">
             <div className="row">
               <div className="col-12 col-xl-6">
                 <h1 className="section__title section__title--head">
-                  {theme.theme.name}
+                  {items.theme.name}
                 </h1>
               </div>
               <div className="col-12">
                 <div className="row row--grid">
-                  {theme.data.movies.map((movie) => (
+                  {items.data.movies.map((movie) => (
                     <div key={movie.slug} className="col-6 col-sm-4 col-lg-3 col-xl-2">
                      <div className="card">
-                        <a href={ movie.type === 'single' ? 'movie/' + movie.slug : 'tv-series/' + movie.slug } className="card__cover">
+                        <a href={ movie.type === 'single' ? '/movie/' + movie.slug : '/tv-series/' + movie.slug } className="card__cover">
                           <img src={ 'https://wsrv.nl/?url=' + movie.image.poster + '&format=webp&quality=50&output=webp' } alt={movie.slug}/>
                           <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" clipRule="evenodd" d="M11 1C16.5228 1 21 5.47716 21 11C21 16.5228 16.5228 21 11 21C5.47716 21 1 16.5228 1 11C1 5.47716 5.47716 1 11 1Z" strokeLinecap="round" strokeLinejoin="round"/>
@@ -155,7 +191,7 @@ export default function MovieThemeLoader() {
                           {movie.rating}
                         </span>
                         <h3 className="card__title">
-                          <a href={ movie.type === 'single' ? 'movie/' + movie.slug : 'tv-series/' + movie.slug }>
+                          <a href={ movie.type === 'single' ? '/movie/' + movie.slug : '/tv-series/' + movie.slug }>
                             {movie.name}
                           </a>
                         </h3>
@@ -170,12 +206,12 @@ export default function MovieThemeLoader() {
                 </div>
               </div>
               <div className="row">
-                <div className="col-12" style={{ textAlign: 'center', marginTop: '20px' }}>
-                  {theme.data.page < theme.data.total_pages && (
+                <div className="col-12">
+                  {items.data.page < items.data.total_pages && (
                     <button
                       className="catalog__more"
                       type="button"
-                      onClick={() => fetchMoreMovies(theme.theme.id, theme.data.page)}>
+                      onClick={() => fetchMoreMovies(items.theme.id, items.data.page)}>
                       Load more
                     </button>
                   )}
@@ -184,15 +220,80 @@ export default function MovieThemeLoader() {
             </div>
           </div>
         </div>
+        ):(
+        <section className="section">
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <h2 className="section__title">{items.theme.name}</h2>
+              </div>
+              <div className="col-12">
+                <div className="section__carousel-wrap">
+                    <OwlCarousel className="" {...options}>
+                       {items.data.movies.map((movie) => (
+                        <div className="card">
+                          <a href="#" className="card__cover">
+                            <img src={'https://wsrv.nl/?url=' + movie.image.poster + '&format=webp&quality=50&output=webp'} alt="" />
+                            <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M13.1615 8.05308C13.1615 9.79908 11.7455 11.2141 9.9995 11.2141C8.2535 11.2141 6.8385 9.79908 6.8385 8.05308C6.8385 6.30608 8.2535 4.89108 9.9995 4.89108C11.7455 4.89108 13.1615 6.30608 13.1615 8.05308Z"
+                              stroke-linecap="round" stroke-linejoin="round" />
+                              <path fill-rule="evenodd" clip-rule="evenodd"
+                              d="M9.998 15.3549C13.806 15.3549 17.289 12.6169 19.25 8.05289C17.289 3.48888 13.806 0.750885 9.998 0.750885H10.002C6.194 0.750885 2.711 3.48888 0.75 8.05289C2.711 12.6169 6.194 15.3549 10.002 15.3549H9.998Z"
+                              stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                          </a>
+                          <button className="card__add" type="button">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                              <path
+                              d="M16,2H8A3,3,0,0,0,5,5V21a1,1,0,0,0,.5.87,1,1,0,0,0,1,0L12,18.69l5.5,3.18A1,1,0,0,0,18,22a1,1,0,0,0,.5-.13A1,1,0,0,0,19,21V5A3,3,0,0,0,16,2Zm1,17.27-4.5-2.6a1,1,0,0,0-1,0L7,19.27V5A1,1,0,0,1,8,4h8a1,1,0,0,1,1,1Z" />
+                            </svg>
+                          </button>
+                          <h3 className="card__title card__title--subs">
+                            <a href="details.html">{movie.name}</a>
+                          </h3>
+                          <ul className="card__list card__list--subs">
+                            <li>More than 300 movies</li>
+                          </ul>
+                        </div>
+                       ))};
+                     
+                    </OwlCarousel>
+                  {/* <div className="section__carousel owl-carousel" id="subscriptions">
+                    
+                  </div> */}
+                  <button className="section__nav section__nav--cards section__nav--prev" data-nav="#subscriptions" type="button">
+                    <svg width="17" height="15" viewBox="0 0 17 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M1.25 7.72559L16.25 7.72559" stroke-width="1.5" stroke-linecap="round"
+                      stroke-linejoin="round" />
+                      <path d="M7.2998 1.70124L1.2498 7.72524L7.2998 13.7502" stroke-width="1.5" stroke-linecap="round"
+                      stroke-linejoin="round" />
+                    </svg>
+                  </button>
+                  <button className="section__nav section__nav--cards section__nav--next" data-nav="#subscriptions" type="button">
+                    <svg width="17" height="15" viewBox="0 0 17 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M15.75 7.72559L0.75 7.72559" stroke-width="1.5" stroke-linecap="round"
+                      stroke-linejoin="round" />
+                      <path d="M9.7002 1.70124L15.7502 7.72524L9.7002 13.7502" stroke-width="1.5" stroke-linecap="round"
+                      stroke-linejoin="round" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+	      </section>
+        )}
+        </>
       ))}
       
       {/* Observer target for infinite scroll */}
       <div ref={observerTarget}></div>
       {loadingThemes && (
-        <div className="loading-themes">
+        <div className="loading-themes" style={{ textAlign: 'center' }}>
           <p>Loading more themes...</p>
         </div>
       )}
-    </div>
+    </>
   );
 }
