2d27706b34023f68dda49ee563e7886e8044978c
dangLuan01 <luanboss012@gmail.com>
2025-12-08 10:10:25 +0700

    fix layout img episode

--- src/components/VideoTVPlayer.jsx
+++ src/components/VideoTVPlayer.jsx
@@ -238,13 +238,15 @@ const VideoPlayer   = ({ movie, thumbnail }) => {
                   className="series"
                   onClick={() => setCurrentEpisodeIdx(epIdx)}>
                   <div className="series__cover">
-                    <img 
-                      src={ imgSrc + movie.image.thumb } 
+                    <div className='thumb-wrapper'>
+                      <img
+                      src={ imgSrc + movie.image.thumb }
                       alt={`Episode ${epIdx + 1}`} 
                       onError={(e) => {
                         e.target.src = 'img/series/2.jpg';
                       }}
                       decoding='sync'/>
+                    </div>
                     <span>
                       {epIdx === currentEpisodeIdx ? 
                       <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
